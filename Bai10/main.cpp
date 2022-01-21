e458adfcbd541aef790e65037f4e9c23bfd1d0d3
maxminlevel <vvtoan35@gmail.com>
2022-01-21 11:14:08 +0700

    bai 15

--- Bai10/main.cpp
+++ Bai10/main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-vector<int> color_layer(MAX, 0);
+vector<int> cnt_layer(MAX, 0);
 
 struct Segment
 {
@@ -29,41 +29,31 @@ int main()
 {
     // space complexity: O(n)
     // time complexity: O(nlog(n))
-
-    ifstream file;
-    file.open("input.txt");
-
-    if (!file.is_open())
-    {
-        cout << "File not found!" << endl;
-        return 0;
-    }
+    // freopen("input.txt", "r", stdin);
 
     int n, max_layer = 0;
-    file >> n;
+    cin >> n;
     vector<Segment> segments(n);
 
     for (int i = 0; i < n; ++i)
     {
         segments[i].id = i;
-        file >> segments[i].a >> segments[i].b;
-        ++color_layer[segments[i].a];
-        --color_layer[segments[i].b + 1];
+        cin >> segments[i].a >> segments[i].b;
+        ++cnt_layer[segments[i].a];
+        --cnt_layer[segments[i].b + 1];
     }
-
-    file.close();
 
     for (int i = 1; i < MAX; ++i)
     {
-        color_layer[i] += color_layer[i - 1];
-        max_layer = max(max_layer, color_layer[i]);
+        cnt_layer[i] += cnt_layer[i - 1];
+        max_layer = max(max_layer, cnt_layer[i]);
     }
 
     cout << "\nResult:\n";
     cout << max_layer << endl;
 
     sort(segments.begin(), segments.end(), compare);
-    vector<int> color_code(n);
+    vector<int> color(n);
     priority_queue<pii, vector<pii>, compare_pii> pq;
 
     for (int i = 0; i < max_layer; ++i)
@@ -73,11 +63,11 @@ int main()
     {
         pii end_point = pq.top();
         pq.pop();
-        color_code[segments[i].id] = end_point.second;
+        color[segments[i].id] = end_point.second;
         pq.push(pii(segments[i].b, end_point.second));
     }
 
     for (int i = 0; i < n; ++i)
-        cout << color_code[i] << " ";
+        cout << color[i] << " ";
     return 0;
 }
--- Bai15/main.cpp
+++ Bai15/main.cpp
@@ -1,47 +1,22 @@
 #include <bits/stdc++.h>
+#include <iomanip>
 using namespace std;
 
-double acos(double x)
+double areaOfIntersection(int &x1, int &y1, int &x2, int &y2, int &R, int dis)
 {
-    return (-0.69813170079773212 * x * x - 0.87266462599716477) * x + 1.5707963267948966;
-}
-
-float Q_rsqrt(float number)
-{
-    long i;
-    float x2, y;
-    const float threehalfs = 1.5F;
-
-    x2 = number * 0.5F;
-    y = number;
-    i = *(long *)&y;           // evil floating point bit level hacking
-    i = 0x5f3759df - (i >> 1); // what the fuck?
-    y = *(float *)&i;
-    y = y * (threehalfs - (x2 * y * y)); // 1st iteration
-                                         //	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
-
-    return y;
-}
-
-double areaOfIntersection(int &x1, int &y1, int &x2, int &y2, int &R, int sqrDistance)
-{
-    double distance = Q_rsqrt(sqrDistance);
-    if (distance >= 2 * R)
-    {
-        return 0;
-    }
-    else
-    {
-        double cosHalfAlpha = distance / (2 * R);
-        double Alpha = 2 * acos(cosHalfAlpha);
-        return Alpha * R * R - R * sin(Alpha);
-    }
+    double cosHalfAlpha = dis / (2 * R);
+    double Alpha = 2 * acos(cosHalfAlpha);
+    return Alpha * R * R - R * R * sin(Alpha);
 }
 double areaOfUnion(int &x1, int &y1, int &x2, int &y2, int &R)
 {
-    int sqrDistance;
-    sqrDistance = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
-    double intersectionArea = areaOfIntersection(x1, y1, x2, y2, R, sqrDistance);
+    int dis;
+    dis = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    if (dis >= 2 * R)
+    {
+        return 0;
+    }
+    double intersectionArea = areaOfIntersection(x1, y1, x2, y2, R, dis);
     return 2 * M_PI * R * R - intersectionArea;
 }
 double round(double val)
@@ -57,16 +32,15 @@ int main()
     int R;
     cin >> x1 >> y1 >> x2 >> y2 >> R;
 
-    // overlap with R1 = R2 = R
     if (x1 == x2 && y1 == y2)
     {
         cout << M_PI * R * R;
     }
     else
     {
-        double unionArea = areaOfUnion(x1, y1, x2, y2, R);
-        double result = round(unionArea * 1000.0) / 1000.0;
-        printf("%.3f", result);
+        // double unionArea = areaOfUnion(x1, y1, x2, y2, R);
+        // double result = round(unionArea * 1000.0) / 1000.0;
+        cout << setprecision(3) << fixed << areaOfUnion(x1, y1, x2, y2, R);
     }
     return 1;
 }
