984f06d45c4d1ec3feec3f67b6d7ae4564e85759
nguyenkhoa <nguyenkhoa0721@gmail.com>
2022-01-21 11:35:25 +0700

    fix bai 20

--- Bai14/main.cpp
+++ Bai14/main.cpp
@@ -1,104 +1,70 @@
-
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
 using namespace std;
-#define MAXN 1000 + 3
-#define MAX 1e9 + 3
-typedef pair<int, int> pii;
-typedef vector<pii> vpii;
-float CalculateConvexPolygonArea(vpii polygon)
+
+struct Pair
 {
-    int n = polygon.size() - 1;
-    float result = (polygon[n].first * polygon[0].first) - (polygon[n].second * polygon[0].first);
-    for (int i = 0; i < n; i++)
+    int x1;
+    int x2;
+};
+
+// (X[i], Y[i]) are coordinates of i'th point.
+double polygonArea(double X[], double Y[], int l, int r)
+{
+
+    double area = 0.0;
+
+    //tinh dien tich theo cong thuc shoelace
+    int j = r;
+    for (int i = l; i <= r; i++)
     {
-        result += (polygon[i].first * polygon[i + 1].second) - (polygon[i].second * polygon[i + 1].first);
+        area += (X[j] + X[i]) * (Y[j] - Y[i]);
+        j = i; // j bang dinh truoc do cua i
     }
-    return abs(result) / 2;
+
+    return abs(area / 2.0);
 }
-float CalculateDifferenceOf2Polygons(vpii polygon, int i, int j)
+
+Pair minDiffArea(double X[], double Y[], int n)
 {
-    vpii pol1;
-    for (int k = i; k <= j; k++)
+    double min = 10000000;
+    Pair res;
+    double area1;
+    double area2;
+    for (int i = 0; i < n - 1; i++)
     {
-        pol1.push_back(polygon[k]);
-    }
-    vpii pol2;
-    for (int k = j; k < polygon.size(); k++)
-    {
-        pol2.push_back(polygon[k]);
-    }
-    for (int k = 0; k <= i; k++)
-    {
-        pol2.push_back(polygon[k]);
-    }
-    float area1 = CalculateConvexPolygonArea(pol1);
-    float area2 = CalculateConvexPolygonArea(pol2);
-    return abs(area1 - area2);
-}
-pii solution(vpii polygon)
-{
-    float min = MAX;
-    float diff_ = min;
-    pii result = make_pair(0, 0);
-    float index_left, index_right;
-    int n = polygon.size() - 1;
-    int right;
-
-    for (int i = 0; i < n; i++)
-    {
-        int mid = (n - i) / 2 + i + 1;
-        float diff = CalculateDifferenceOf2Polygons(polygon, i, mid);
-        if (diff < min)
+        for (int j = i + 2; j < n; j++)
         {
-            min = diff;
-            result = {i, mid};
-        }
-        right = 1;
-        index_right = (mid + right) % (n + 1);
-        while (diff_ < min && index_right <= i - 2)
-        {
-            right++;
-            index_right = (mid + right) % (n + 1);
-            diff_ = CalculateDifferenceOf2Polygons(polygon, i, index_right);
-
-            if (diff_ > min)
+            if (i == 0 && j == n - 1)
                 break;
-
-            if (diff_ < min)
+            area1 = polygonArea(X, Y, i, j);
+            area2 = polygonArea(X, Y, 0, n - 1) - area1;
+            if (abs(area1 - area2) < min)
             {
-                min = diff_;
-                result = {i, index_right};
-            }
-        }
-        index_left = mid;
-        while (diff_ < min && index_left >= i + 2)
-        {
-            index_left++;
-            diff_ = CalculateDifferenceOf2Polygons(polygon, i, index_left);
-            if (diff_ > min)
-                break;
-            if (diff_ < min)
-            {
-                min = diff_;
-                result = {i, index_left};
+                min = abs(area1 - area2);
+                res.x1 = i;
+                res.x2 = j;
+                //cout << i <<" "<< j << " "<< area1 <<" "<< area2 <<endl ;
             }
         }
     }
-    result.first++;
-    result.second++;
-    return result;
+
+    return res;
 }
+
+// Driver program to test above function
 int main()
 {
     int n;
-    vpii polygon;
+    double X[1000];
+    double Y[1000];
+    Pair res;
+    cout << "Nhap n: ";
+    cin >> n;
     for (int i = 0; i < n; i++)
     {
-        int a, b;
-        cin >> a >> b;
-        polygon.push_back(make_pair(a, b));
+        cin >> X[i] >> Y[i];
     }
-    pii result = solution(polygon);
-    cout << result.first << " " << result.second;
-    return 0;
+    res = minDiffArea(X, Y, n);
+    cout << res.x1 << " " << res.x2 << endl;
 }
--- Bai15/main.cpp
+++ Bai15/main.cpp
@@ -1,31 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-double acos(double x)
-{
-    return (-0.69813170079773212 * x * x - 0.87266462599716477) * x + 1.5707963267948966;
-}
-
-float Q_rsqrt(float number)
-{
-    long i;
-    float x2, y;
-    const float threehalfs = 1.5F;
-
-    x2 = number * 0.5F;
-    y = number;
-    i = *(long *)&y;           // evil floating point bit level hacking
-    i = 0x5f3759df - (i >> 1); // what the fuck?
-    y = *(float *)&i;
-    y = y * (threehalfs - (x2 * y * y)); // 1st iteration
-                                         //	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
-
-    return y;
-}
-
 double areaOfIntersection(int &x1, int &y1, int &x2, int &y2, int &R, int sqrDistance)
 {
-    double distance = Q_rsqrt(sqrDistance);
+    double distance = sqrt(sqrDistance);
     if (distance >= 2 * R)
     {
         return 0;
@@ -52,7 +30,7 @@ double round(double val)
 }
 int main()
 {
-    // freopen("input.txt", "r", stdin);
+    freopen("input.txt", "r", stdin);
     int x1, y1, x2, y2;
     int R;
     cin >> x1 >> y1 >> x2 >> y2 >> R;
