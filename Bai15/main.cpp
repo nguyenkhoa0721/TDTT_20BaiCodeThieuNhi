83ee94a487b0d84b09038047cc6569e2e76de447
nguyenkhoa <nguyenkhoa0721@gmail.com>
2022-01-23 12:54:42 +0700

    final fix

--- Bai15/main.cpp
+++ Bai15/main.cpp
@@ -1,25 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-int x1, y1, x2, y2, R;
+int x_1, y_1, x_2, y_2, R;
 double dis, alpha;
 
 double areaOfIntersection()
 {
-    alpha = 2 * acos(dis / (2 * R));
+    alpha = 2.0 * acos(dis / (2.0 * R));
     return alpha * R * R - R * R * sin(alpha);
 }
 double solution()
 {
-    int dis = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    dis = sqrt((x_1 - x_2) * (x_1 - x_2) + (y_1 - y_2) * (y_1 - y_2));
     return dis >= 2 * R ? 2 * M_PI * R * R : 2 * M_PI * R * R - areaOfIntersection();
 }
 int main()
 {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    cin >> x1 >> y1 >> x2 >> y2 >> R;
+    
+    cin >> x_1 >> y_1 >> x_2 >> y_2 >> R;
 
-    if (x1 == x2 && y1 == y2)
+    if (x_1 == x_2 && y_1 == y_2)
     {
         cout << M_PI * R * R;
     }
